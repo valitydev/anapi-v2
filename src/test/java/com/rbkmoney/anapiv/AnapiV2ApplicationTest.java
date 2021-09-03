4d18a37ac7be90ba6c176e761078749ca1d90bc5
echerniak <cherniak3@yandex.ru>
2021-09-03 14:30:01 +0300

    Template generated

---
+++ src/test/java/com/rbkmoney/anapiv/AnapiV2ApplicationTest.java
@@ -0,0 +1,16 @@
+package com.rbkmoney.anapiv;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = AnapiV2Application.class)
+public class AnapiV2ApplicationTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
