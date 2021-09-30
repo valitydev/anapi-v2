36b68761bae7cfb4bbf201eab9cdcb5557d4ea64
echerniak <cherniak3@yandex.ru>
2021-09-30 16:05:22 +0300

    WIP. Refactor

---
+++ src/main/java/com/rbkmoney/anapi/v2/exception/AuthorizationException.java
@@ -0,0 +1,8 @@
+package com.rbkmoney.anapi.v2.exception;
+
+public class AuthorizationException extends RuntimeException {
+
+    public AuthorizationException(String s) {
+        super(s);
+    }
+}
--- src/main/java/com/rbkmoney/anapi/v2/security/BouncerAccessService.java
+++ src/main/java/com/rbkmoney/anapi/v2/security/BouncerAccessService.java
@@ -1,9 +1,11 @@
 package com.rbkmoney.anapi.v2.security;
 
+import com.rbkmoney.anapi.v2.exception.AuthorizationException;
 import com.rbkmoney.anapi.v2.service.KeycloakService;
 import com.rbkmoney.bouncer.starter.api.BouncerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.keycloak.representations.AccessToken;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -16,27 +18,30 @@ public class BouncerAccessService implements AccessService {
     private final KeycloakService keycloakService;
 
     @Value("${bouncer.auth.enabled}")
-    private Boolean authEnabled;
+    private boolean authEnabled;
 
     @Override
     public void checkAccess(String operationId, String partyId, String shopId) {
-        log.info("Check the user's rights to perform the operation $operationId");
+        log.info("Check the user's rights to perform the operation {}", operationId);
         var ctx = buildAnapiBouncerContext(operationId, partyId, shopId);
         if (!bouncerService.havePrivileges(ctx)) {
             if (authEnabled) {
-                //TODO: custom exception;
+                throw new AuthorizationException(String.format("No rights to perform %s", operationId));
             } else {
-                log.warn("No rights to perform $operationId");
+                log.warn("No rights to perform {}", operationId);
             }
         }
     }
 
     private AnapiBouncerContext buildAnapiBouncerContext(String operationId, String partyId, String shopId) {
-        //TODO: Keycloak fields
+        AccessToken token = keycloakService.getAccessToken();
         return AnapiBouncerContext.builder()
                 .operationId(operationId)
                 .partyId(partyId)
                 .shopId(shopId)
+                .tokenExpiration(token.getExp())
+                .tokenId(token.getId())
+                .userId(token.getSubject())
                 .build();
     }
 }
