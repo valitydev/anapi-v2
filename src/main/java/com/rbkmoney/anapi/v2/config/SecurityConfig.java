7d4e188a3cfd5903b184064c15ad3a3c0e5550e9
echerniak <cherniak3@yandex.ru>
2021-11-24 16:24:58 +0300

    Circular beans error fix

--- src/main/java/com/rbkmoney/anapi/v2/config/SecurityConfig.java
+++ src/main/java/com/rbkmoney/anapi/v2/config/SecurityConfig.java
@@ -1,14 +1,8 @@
 package com.rbkmoney.anapi.v2.config;
 
-import lombok.SneakyThrows;
-import org.keycloak.adapters.KeycloakConfigResolver;
-import org.keycloak.adapters.KeycloakDeployment;
-import org.keycloak.adapters.KeycloakDeploymentBuilder;
 import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
-import org.keycloak.representations.adapters.config.AdapterConfig;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -21,15 +15,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
-import org.springframework.util.StringUtils;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
@@ -41,27 +29,6 @@ import java.util.stream.Collectors;
 @EnableGlobalMethodSecurity(prePostEnabled = true, proxyTargetClass = true)
 @ConditionalOnProperty(value = "auth.enabled", havingValue = "true")
 public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
-
-    @Value("${keycloak.realm}")
-    private String keycloakRealmName;
-
-    @Value("${keycloak.resource}")
-    private String keycloakResourceName;
-
-    @Value("${keycloak.realm-public-key}")
-    private String keycloakRealmPublicKey;
-
-    @Value("${keycloak.realm-public-key.file-path:}")
-    private String keycloakRealmPublicKeyFile;
-
-    @Value("${keycloak.auth-server-url}")
-    private String keycloakAuthServerUrl;
-
-    @Value("${keycloak.ssl-required}")
-    private String keycloakSSLRequired;
-
-    @Value("${keycloak.not-before}")
-    private int keycloakTokenNotBefore;
 
     @Bean
     @Override
@@ -87,15 +54,6 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public KeycloakConfigResolver keycloakConfigResolver() {
-        return facade -> {
-            KeycloakDeployment deployment = KeycloakDeploymentBuilder.build(adapterConfig());
-            deployment.setNotBefore(keycloakTokenNotBefore);
-            return deployment;
-        };
-    }
-
-    @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.applyPermitDefaultValues();
@@ -104,29 +62,5 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
-    }
-
-    private AdapterConfig adapterConfig() {
-        if (StringUtils.hasLength(keycloakRealmPublicKeyFile)) {
-            keycloakRealmPublicKey = readKeyFromFile(keycloakRealmPublicKeyFile);
-        }
-
-        AdapterConfig adapterConfig = new AdapterConfig();
-        adapterConfig.setRealm(keycloakRealmName);
-        adapterConfig.setRealmKey(keycloakRealmPublicKey);
-        adapterConfig.setResource(keycloakResourceName);
-        adapterConfig.setAuthServerUrl(keycloakAuthServerUrl);
-        adapterConfig.setUseResourceRoleMappings(true);
-        adapterConfig.setBearerOnly(true);
-        adapterConfig.setSslRequired(keycloakSSLRequired);
-        return adapterConfig;
-    }
-
-    @SneakyThrows
-    private String readKeyFromFile(String filePath) {
-        List<String> strings = Files.readAllLines(Paths.get(filePath));
-        strings.remove(strings.size() - 1);
-        strings.remove(0);
-        return strings.stream().map(String::trim).collect(Collectors.joining());
     }
 }
