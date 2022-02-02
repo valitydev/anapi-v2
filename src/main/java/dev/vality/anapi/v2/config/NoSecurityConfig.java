74b12157c76d75eb86d48ec63bee7a279378596c
Egor Cherniak <cherniak3@yandex.ru>
2022-02-02 16:46:49 +0300

    Fix prometheus endpoint access

---
+++ src/main/java/dev/vality/anapi/v2/config/NoSecurityConfig.java
@@ -0,0 +1,17 @@
+package dev.vality.anapi.v2.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@ConditionalOnProperty(value = "auth.enabled", havingValue = "false")
+public class NoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+    }
+
+}
