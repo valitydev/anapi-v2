2b574330949655c8bcb48964898d0afcbaef8819
Egor Cherniak <cherniak3@yandex.ru>
2022-08-15 11:10:28 +0300

    Remove nullable + bump plugins (#48)

--- src/main/java/dev/vality/anapi/v2/security/AccessData.java
+++ src/main/java/dev/vality/anapi/v2/security/AccessData.java
@@ -11,7 +11,6 @@ import java.util.List;
 public class AccessData {
 
     private final String operationId;
-    @Nullable
     private final String partyId;
     @Nullable
     private final List<String> shopIds;
